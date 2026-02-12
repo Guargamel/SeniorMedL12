09aa79e8996319cea481b574452a24a92495f4b5
Guargamel <233357057+Guargamel@users.noreply.github.com>
2026-02-13 00:37:09 +0800

    web routes

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
 
 // Serve the SPA for all non-API routes
-Route::view('/{any}', 'app')->where('any', '^(?!api).*$');
+// Route::view('/{any}', 'app')->where('any', '^(?!api).*$');
+
+Route::get('/dashboard', function () {
+    return view('app'); // or whatever blade loads your React SPA (often resources/views/app.blade.php)
+})->name('dashboard');
+
+Route::get('/{any}', function () {
+    return view('app');
+})->where('any', '.*');
