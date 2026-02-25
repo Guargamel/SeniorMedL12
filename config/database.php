fad23e60d61355dfc5238152175d6c9620ed0041
Guargamel <233357057+Guargamel@users.noreply.github.com>
2026-02-25 21:22:07 +0800

    database and .env changes attemtping to fix SQLSTATE[08006] [7] connection to server at

--- config/database.php
+++ config/database.php
@@ -58,8 +58,9 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                (PHP_VERSION_ID >= 80500 ? \Pdo\Mysql::ATTR_SSL_CA : \PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
+            'options' => extension_loaded('pdo_pgsql') ? array_filter([
+                \PDO::ATTR_TIMEOUT => 5,
+                \PDO::PGSQL_ATTR_DISABLE_PREPARES => true,
             ]) : [],
         ],
 
@@ -148,7 +149,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug((string) env('APP_NAME', 'laravel')).'-database-'),
+            'prefix' => env('REDIS_PREFIX', Str::slug((string) env('APP_NAME', 'laravel')) . '-database-'),
             'persistent' => env('REDIS_PERSISTENT', false),
         ],
 
